e0d139b9bd0f3f86b134844e21c0690b83988804
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-11-28 15:29:30 -0500

    perf: :zap: Created helper function for starting jobs

--- lib/jobber.ex
+++ lib/jobber.ex
@@ -3,5 +3,9 @@ defmodule Jobber do
   Documentation for `Jobber`.
   """
 
+  alias Jobber.{JobRunner, Job}
 
+  def start_job(args) do
+    DynamicSupervisor.start_child(JobRunner, {Job, args})
+  end
 end
